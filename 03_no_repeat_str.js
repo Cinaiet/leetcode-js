33b055563b990ecfd64d65369eadd64f8f548c4e
王冠伟 <crazyguanwei@163.com>
2021-04-13 22:34:59 +0800

    day02

---
+++ 03_no_repeat_str.js
@@ -0,0 +1,36 @@
+/**
+ * 
+ * 3. 无重复字符的最长子串
+ * 
+ * 输入: s = "abcabcbb"
+ * 输出: 3 
+ * 解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3
+ * 
+ * 
+*/
+
+
+const str = "abcabcbb"
+
+var lengthOfLongestSubstring = function(s) {
+  let sourceLen = s.length
+  if(!sourceLen) return 0
+  
+  let target = '', len = 0, index = 0
+  let i = 0
+  for(; i < sourceLen; i++) {
+    let temp = s[i]
+    if(target.includes(temp)) {
+      let tempLen = target.length
+      if(tempLen > len) len = tempLen
+      target = ''
+      i = index
+    } else {
+      if(target === '') index = i
+      target += temp
+    }
+  }
+  return target.length > len ? target.length : len
+}
+
+console.log(lengthOfLongestSubstring(str))
